b62046ee054b3bdccaf3f670121b32ec9e2fc7a2
szagriichuk <s.zagriichuk@datacradle.com>
2012-12-06 12:52:05 -0800

    Implementation of PDfEngine

--- src/main/java/com/google/text2pdf/TextToPdfEngine.java
+++ src/main/java/com/google/text2pdf/TextToPdfEngine.java
@@ -1,13 +1,51 @@
 package com.google.text2pdf;
 
+import com.google.text2pdf.logger.LoggerFacade;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import org.slf4j.Logger;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 /**
  * Hello world!
- *
  */
-public class TextToPdfEngine
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class TextToPdfEngine {
+
+    private static final Logger LOGGER = LoggerFacade.getLogger(TextToPdfEngine.class);
+
+    public void convertTextToPdf(String text, String pathToPdfFile) throws FileNotFoundException {
+        Document document = new Document();
+
+        createPdfWriterForDocument(pathToPdfFile, document);
+
+        document.open();
+
+        addParagraphWithDataToDocument(document, text);
+
+        document.close();
+    }
+
+    private void addParagraphWithDataToDocument(Document document, String text) {
+        try {
+            document.add(new Paragraph(text));
+        } catch (DocumentException e) {
+            LOGGER.error(e.getMessage());
+        }
+    }
+
+    private void createPdfWriterForDocument(String pathToPdfFile, Document document) throws FileNotFoundException {
+
+        if (pathToPdfFile == null)
+            throw new FileNotFoundException("Path to document cannot be null");
+
+        try {
+            PdfWriter.getInstance(document, new FileOutputStream(pathToPdfFile));
+        } catch (DocumentException e) {
+            LOGGER.error(e.getMessage());
+        }
     }
 }
--- src/test/java/com/google/text2pdf/TextToPdfEngineTest.java
+++ src/test/java/com/google/text2pdf/TextToPdfEngineTest.java
@@ -1,38 +1,101 @@
 package com.google.text2pdf;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfReaderContentParser;
+import com.itextpdf.text.pdf.parser.SimpleTextExtractionStrategy;
+import com.itextpdf.text.pdf.parser.TextExtractionStrategy;
+import junit.framework.Assert;
+import org.junit.Test;
+
+import java.io.*;
 
 /**
  * Unit test for simple TextToPdfEngine.
  */
-public class TextToPdfEngineTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public TextToPdfEngineTest(String testName)
-    {
-        super( testName );
+public class TextToPdfEngineTest {
+    private TextToPdfEngine textToPdfEngine = new TextToPdfEngine();
+
+    @Test(expected = com.itextpdf.text.ExceptionConverter.class)
+    public void testNullTextToPdf() throws Exception {
+        textToPdfEngine.convertTextToPdf(null, "test1.pdf");
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( TextToPdfEngineTest.class );
+    @Test(expected = FileNotFoundException.class)
+    public void testNullPathToPdf() throws Exception {
+        textToPdfEngine.convertTextToPdf("test content", null);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test(expected = com.itextpdf.text.ExceptionConverter.class)
+    public void testEmptyTextToPdf() throws Exception {
+
+        String fileName = "test1.pdf";
+
+        textToPdfEngine.convertTextToPdf("", fileName);
+
+        Assert.assertEquals("", readDataFromPdf(fileName));
     }
+
+    @Test(expected = FileNotFoundException.class)
+    public void testEmptyPathToPdf() throws Exception {
+        textToPdfEngine.convertTextToPdf("test content", "");
+    }
+
+    @Test
+    public void simpleTextToPdf() throws Exception {
+        testWriteContentToPdf("simplePdfFile.pdf", "./src/test/java/simpleTestData.txt");
+    }
+
+    @Test
+    public void middleTextToPdf() throws Exception {
+
+        testWriteContentToPdf("middlePdfFile.pdf", "./src/test/java/middleTestData.txt");
+    }
+
+    @Test
+    public void bigTextToPdf() throws Exception {
+
+        testWriteContentToPdf("bigPdfFile.pdf", "./src/test/java/bigTextData.txt");
+    }
+
+    private void testWriteContentToPdf(String pdfFileName, String textContentFileName) throws IOException {
+
+        String data = readDataFromTextFile(textContentFileName);
+
+        textToPdfEngine.convertTextToPdf(data, pdfFileName);
+
+        Assert.assertEquals(data, readDataFromPdf(pdfFileName));
+    }
+
+    private String readDataFromTextFile(String pathToFile) throws IOException {
+        InputStream stream = new FileInputStream(pathToFile);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+        readContentToOutStream(stream, outputStream);
+
+        return new String(outputStream.toByteArray()).trim();
+    }
+
+    private void readContentToOutStream(InputStream stream, ByteArrayOutputStream outputStream) throws IOException {
+
+        byte[] data = new byte[1024];
+
+        while (stream.read(data) > -1) {
+            outputStream.write(data);
+        }
+    }
+
+
+    private String readDataFromPdf(String fileName) throws IOException {
+        PdfReader pdfReader = new PdfReader(fileName);
+        PdfReaderContentParser parser = new PdfReaderContentParser(pdfReader);
+        TextExtractionStrategy strategy;
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 1; i <= pdfReader.getNumberOfPages(); i++) {
+            strategy = parser.processContent(i, new SimpleTextExtractionStrategy());
+            builder.append(strategy.getResultantText());
+        }
+        return builder.toString().trim();
+    }
+
 }
